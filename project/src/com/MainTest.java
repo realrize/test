eb744b298d3dc07f8006b96b1b2a6052515a3d53
rockmaxi <gmail.com>
2019-09-18 11:21:18 +0900

    master

--- project/src/com/MainTest.java
+++ project/src/com/MainTest.java
@@ -26,8 +26,8 @@ public class MainTest {
         System.out.println(list);*/
 
         //Arrays.asList(1, 2, 3).stream().sorted(Comparator.<Integer>reverseOrder()).forEach(System.out::println); // 1,2,3
-        //test abc
-        //test branch2
+        //abc
+        //branch2
 
         Stream<Integer> s1  = Stream.of(1, 11, 111, 1111)
                 .peek(e -> System.out.println("Filtered value: " + e));
